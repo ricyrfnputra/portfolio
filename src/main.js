650fbf16d05b3eb7e76a11fd331b5f1dcff77014
Ricy <ricyrfnputra51@gmail.com>
2026-01-16 18:59:00 +0700

    add footer section

--- src/main.js
+++ src/main.js
@@ -4,11 +4,10 @@ import './style.css'
 const nav = document.querySelector('nav');
   window.addEventListener('scroll', () => {
     if (window.scrollY > 50) {
-      // Saat di-scroll kebawah: tambah shadow dan border violet
       nav.classList.add('shadow-[0_10px_20px_-5px_rgba(139,92,246,0.4)]', 'border-b', 'border-violet-500/50');
       nav.classList.remove('shadow-none');
-    } else {
-      // Saat kembali ke paling atas: hapus shadow
+    } 
+    else {
       nav.classList.remove('shadow-[0_10px_20px_-5px_rgba(139,92,246,0.4)]', 'border-b', 'border-violet-500/50');
       nav.classList.add('shadow-none');
     }
